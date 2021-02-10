c441b3f973ee6c1c4a43b70f86d74c9ef5e77432
sriramho <46807608+sriramho@users.noreply.github.com>
2021-02-10 19:57:55 +0100

    addIndex

--- src/pages/app.js
+++ src/pages/app.js
@@ -5,6 +5,7 @@ import Details from "../components/Details"
 import Home from "../components/Home"
 import Login from "../components/Login"
 import SignUp from "../components/SignUp"
+import Index from "../components.Index.html"
 import PrivateRoute from "../components/PrivateRoute"
 
 
@@ -13,6 +14,7 @@ const App = () => (
     <Router>
       <PrivateRoute path="/app/home" component={Home} />
       <PrivateRoute path="/app/profile" component={Details} />
+      <PrivateRoute path="/app/index" component={Index} />
       <Login path="/app/login" />
       <SignUp path="/app/signup" />
     </Router>
